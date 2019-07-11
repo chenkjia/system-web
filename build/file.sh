e1dc7fbb46a92aba1d27f1a8ac83acefe881cc5b
chenkjia <chenkj@gddxit.com>
2019-07-11 17:33:03 +0800

    版本格式改变

--- build/file.sh
+++ build/file.sh
@@ -2,7 +2,7 @@
 env=`cat version.json | jq -r '.env'`
 version=`cat version.json | jq -r '.version'`
 subversion=`cat version.json | jq -r '.subversion'`
-myPath="archives/V-$env-$version.$subversion"
+myPath="archives/$env-V$version.$subversion"
 projectPath="archives/"
 if [ ! -x "$projectPath" ]; then 
 mkdir "$projectPath"  
--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://192.168.0.66:3000'
+        target: 'http://localhost:3000'
       }
     }
   },
@@ -26,7 +26,7 @@ module.exports = {
       filename: 'index.html',
       // 当使用 title 选项时，
       // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
-      version: `V-${version.env}-${version.version}.${version.subversion}`,
+      version: `${version.env}-V${version.version}.${version.subversion}`,
       // 在这个页面中包含的块，默认情况下会包含
       // 提取出来的通用 chunk 和 vendor chunk。
       chunks: ['chunk-vendors', 'chunk-common', 'index']
