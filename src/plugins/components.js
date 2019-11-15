401bae2e16f44c6625c10ce7f8f146350b5f7b0a
chenkj <>
2019-11-15 23:16:05 +0800

    外部引入element

--- src/plugins/components.js
+++ src/plugins/components.js
@@ -1,23 +1,25 @@
 import Vue from 'vue'
-import {
-  Button,
-  Dialog,
-  MessageBox,
-  Container,
-  Header,
-  Aside,
-  Main,
-  Upload,
-  Table,
-  TableColumn,
-  Pagination,
-  Form,
-  FormItem,
-  Input,
-  Switch,
-  Select,
-  Image
-} from 'element-ui'
+// import ElementUI from 'element-ui'
+
+// import {
+//   Button,
+//   Dialog,
+//   MessageBox,
+//   Container,
+//   Header,
+//   Aside,
+//   Main,
+//   Upload,
+//   Table,
+//   TableColumn,
+//   Pagination,
+//   Form,
+//   FormItem,
+//   Input,
+//   Switch,
+//   Select,
+//   Image
+// } from 'element-ui'
 
 import {
   ButtonGroup,
@@ -26,28 +28,29 @@ import {
   DatatablesPage
 } from '@/components'
 
-Vue.use(Button)
-Vue.use(Dialog)
+// Vue.use(Button)
+// Vue.use(Dialog)
 // Vue.use(MessageBox)
-Vue.use(Container)
-Vue.use(Header)
-Vue.use(Aside)
-Vue.use(Main)
-Vue.use(Upload)
-Vue.use(Table)
-Vue.use(TableColumn)
-Vue.use(Pagination)
-Vue.use(Form)
-Vue.use(FormItem)
-Vue.use(Input)
-Vue.use(Switch)
-Vue.use(Select)
-Vue.use(Image)
+// Vue.use(Container)
+// Vue.use(Header)
+// Vue.use(Aside)
+// Vue.use(Main)
+// Vue.use(Upload)
+// Vue.use(Table)
+// Vue.use(TableColumn)
+// Vue.use(Pagination)
+// Vue.use(Form)
+// Vue.use(FormItem)
+// Vue.use(Input)
+// Vue.use(Switch)
+// Vue.use(Select)
+// Vue.use(Image)
+// Vue.use(ElementUI)
 
-Vue.prototype.$msgbox = MessageBox.msgbox
-Vue.prototype.$alert = MessageBox.alert
-Vue.prototype.$confirm = MessageBox.confirm
-Vue.prototype.$prompt = MessageBox.prompt
+// Vue.prototype.$msgbox = MessageBox.msgbox
+// Vue.prototype.$alert = MessageBox.alert
+// Vue.prototype.$confirm = MessageBox.confirm
+// Vue.prototype.$prompt = MessageBox.prompt
 
 Vue.use(ButtonGroup)
 Vue.use(Dataform)
