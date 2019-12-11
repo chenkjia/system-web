fa786abeb2fddd597795bc0ffcdb2147c45e72e8
chenkj <>
2019-12-11 18:34:49 +0800

    401和404页面

---
+++ src/views/ErrorPage/401.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  div 401
+</template>
+
+<script>
+
+export default {
+  name: 'Page404'
+}
+</script>
---
+++ src/views/ErrorPage/404.vue
@@ -0,0 +1,10 @@
+<template lang="pug">
+  div 404
+</template>
+
+<script>
+
+export default {
+  name: 'Page404'
+}
+</script>
