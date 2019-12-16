091d71ac994fdb0263d83c01dae3dc010f1d7ed0
chenkj <chenkjia@live.com>
2019-12-16 20:05:54 +0800

    datatables加载时的暂无数据

--- src/components/DatatablesPage/index.vue
+++ src/components/DatatablesPage/index.vue
@@ -23,7 +23,7 @@
         element-loading-text="拼命加载中"
         element-loading-spinner="el-icon-loading"
         element-loading-background="rgba(0, 0, 0, 0.2)"
-        :empty-text="loading?'':'暂无数据'"
+        :empty-text="loading?' ':'暂无数据'"
         :data="tableData",
         :columns="columns"
         @sort-change="sortChange")
