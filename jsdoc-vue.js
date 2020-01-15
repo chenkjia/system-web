83b1e1415462337e5c95b51ddb8f4d115dc08a28
manielam <manielam@163.com>
2020-01-15 16:04:19 +0800

    补充文档解析之后自动打开浏览器，增加jsdoc.README文件

--- jsdoc-vue.js
+++ jsdoc-vue.js
@@ -1,4 +1,5 @@
 var compiler = require('vue-template-compiler')
+const { exec } = require('child_process')
 
 exports.handlers = {
   // 利用 vue-template-compiler 编译 vue 模板
@@ -7,5 +8,17 @@ exports.handlers = {
       var output = compiler.parseComponent(e.source)
       e.source = output.script ? output.script.content : ''
     }
+  },
+  parseComplete: function ({ sourcefiles, doclets }) {
+    // eslint-disable-next-line no-path-concat
+    const url = __dirname + '/doc/index.html'
+    switch (process.platform) {
+      // win系统使用
+      case 'win32': exec(`start ${url}`); break
+      // mac系统使用
+      case 'darwin':
+      default: exec(`open ${url}`)
+    }
+    console.log(`可查看文档：${url}`)
   }
 }
